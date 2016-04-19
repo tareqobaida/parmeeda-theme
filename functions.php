abe1c07d549f180b6f3786438123580a1dd91376
tareqobaida <tareqbuet@gmail.com>
2016-04-19 17:04:28 -0500

    created woocommerce folder in order to modify woocommerce template

--- functions.php
+++ functions.php
@@ -10,3 +10,20 @@ function theme_enqueue_styles() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action( 'init', 'remove_footer_credit', 10 );
+
+function remove_footer_credit () {
+	remove_action( 'storefront_footer', 'storefront_credit', 20 );
+	add_action( 'storefront_footer', 'custom_storefront_credit', 20 );
+}
+
+function custom_storefront_credit() {
+	?>
+	<div class="site-info">
+		&copy; <?php echo get_bloginfo( 'name' ) . ' ' . get_the_date( 'Y' ); ?>
+		<br>
+		Developed by <a href="http://www.whitecanvassoft.com">WhiteCanvasSoftware</a>
+	</div><!-- .site-info -->
+	<?php
+}
+
